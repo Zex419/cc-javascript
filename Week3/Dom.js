72f125979c7dc250420a9aadc35ab931191ae955
Zex419 <wantedsway2@gmail.com>
2024-05-23 13:27:15 +0100

    Use javascript to chnage CSS properties of HTML elements

--- Week3/Dom.js
+++ Week3/Dom.js
@@ -1,4 +1,11 @@
 let paragraphs = document.getElementsByTagName('p');
-for (let item in paragraphs){
-    console
-}
+let heading = document.getElementsByClassName('heading');
+let link = document.getElementById('next-page');
+let firstParagraph = document.getElementById('first');
+//for (let count = 0; count <= paragraphs.length; count++){
+  //  paragraphs[count++].innerHTML = "new content";
+//}
+
+//console.log(heading)
+link.href="https://www.google.com/"
+firstParagraph.style.color = 'red';
