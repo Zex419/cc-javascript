f209843d18e569f9138e1ce068e61e52acf2a26c
Zex419 <wantedsway2@gmail.com>
2024-05-23 13:47:18 +0100

    Use javascript to dynamically add elements to a page

--- Week3/Dom.js
+++ Week3/Dom.js
@@ -2,10 +2,15 @@ let paragraphs = document.getElementsByTagName('p');
 let heading = document.getElementsByClassName('heading');
 let link = document.getElementById('next-page');
 let firstParagraph = document.getElementById('first');
+let topic = document.createElement('h2')
+
 //for (let count = 0; count <= paragraphs.length; count++){
   //  paragraphs[count++].innerHTML = "new content";
 //}
 
 //console.log(heading)
-link.href="https://www.google.com/"
+link.setAttribute("href", "https://ai.google.com/")
 firstParagraph.style.color = 'red';
+topic.innerHTML = 'Third topic';
+paragraphs[1].appendChild(topic);   
+doucument.write('<h1>this is new content</h1>')
